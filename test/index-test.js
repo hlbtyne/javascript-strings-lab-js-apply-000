3da9a07a049a554aaf018397e486e476142dcea8
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-02-11 18:23:19 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,13 @@
+var greeting = "";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "" + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `${topic}`;
+
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
 it('defines `greeting`', function() {
